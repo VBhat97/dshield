658aa4186c1d09a2df82d10469f3a6e8d85cf49b
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-10-06 00:24:12 +0530

    Front end beggining

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="css/jquery.datetimepicker.css" rel="stylesheet" />
+        <link href="css/timepicki.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <center>
+        <img src="img/up_header.png" class="img_dw_a_up img_dw_a">
+    </center>
+    <div class="main-background">
+        <br><br><br>
+        <div class="firstelement">
+            <strong>MYACCOUNT</strong>
+        </div>
+        <br>
+        <div class="secondelement">
+            <p class="secondfirst"><strong class="insider">ACCOUNT ID </strong>: {{ }}</p>
+    </div>
+    
+    </body>
+</html>
