e2422288bf133f582a9869b773fa4c1ba6a0e604
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-10-11 03:08:44 +0530

    Further Progress, backend connected

--- index-script.php
+++ index-script.php
@@ -5,13 +5,25 @@ $con = mysqli_connect("localhost","root","","dshield") or die(mysqli_error($con)
 $fname = $_POST['FirstName'];
 $lname = $_POST['LastName'];
 
-$full_name = $fname . '' . $lname;
+$fullname = $fname . ' ' . $lname;
 
 $age = $_POST['Age'];
 $bgroup = $_POST['BGroup'];
 $hname = $_POST['HName'];
 
 $state = $_POST['State'];
+$city = $_POST['City'];
+$area = $_POST['Area'];
+
+$prelfname = $_POST['PRelFname'];
+$prelfnum = $_POST['PRelFnum'];
+
+$v_GenOTP = rand(100000, 999999);
+
+$user_select_query="Insert into vtbl_bldhlp (BldHlpID,PatName,PatAge,PatBldGrp,PatArea,PatZipCd,PatHospName,PatState,PatCity,PatRelName,PatRelMobNum,GenOTP,ActiveStat,InsDateTime) VALUES (NULL,'$fullname','$age','$bgroup','$area','$area','$hname','$state','$city','$prelfname','$prelfnum','$v_GenOTP',0,NOW())";
+$user_query_result = mysqli_query($con, $user_select_query);
+
+header('Location: index.php')
 
 
 ?>
